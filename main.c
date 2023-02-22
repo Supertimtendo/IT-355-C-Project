96be82c6fdab616ee1f113426bff1e670835faed
Tim <timburanicz@gmail.com>
2023-02-21 23:05:02 -0600

    Created main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ * @file main.c
+ * @author Tim Buranicz
+ * @version 1.0
+ * Main file to run
+ */
+
+/**
+ * Main function
+ * @return Return 0 for exit success
+ */
+int main(){
+    return 0;
+}
