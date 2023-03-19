abdc9f0140260f39daa0134dd17cd9d2e93f9ddd
beebelucas <beebelucas@gmail.com>
2023-03-19 12:44:19 -0500

    Cleaning up recommendations: adding makefile

--- Rules/MSC32.c
+++ Rules/MSC32.c
@@ -8,15 +8,16 @@ Rule: MSC32
 #include<stdio.h>
 #include<time.h>
 
-struct timespec ts;
+
 int main(){
+    struct timespec ts;
     int random_num;
     srandom(ts.tv_nsec ^ ts.tv_sec); //properly seeding the random number generator
 
     //generating 5 random numbers: each time this program is run, different numbers will be printed
-    for(int i = 0; i < 5; i++){
+    for(int i = 1; i <= 5; i++){
         random_num = random(); //generating the random number 
-        printf("Random num %d",random_num);
+        printf("Random num %d: %d\n",i,random_num);
     }
 
     return 0;
