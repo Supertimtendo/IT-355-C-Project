830718aac552645b185c1cbc453502d3793c2292
eddieskettie <91576104+eddieskettie@users.noreply.github.com>
2023-03-20 06:06:23 -0500

    Update bank.h

--- bank.h
+++ bank.h
@@ -26,4 +26,25 @@ void transferFunds(bank *b);
 void deposit(bank *b);
 void withdrawal(bank *b);
 
+/*
+* @brief example of PRE01-C. Use parentheses within macros around parameter names
+* this macro uses the correct parenthesis around parameters, no practical use in this specific program but the rec example is here
+*/
+#define SQUAREMACRO (I) ((I)*(I))
+int squareMacNum = SQUAREMACRO(2);
+/*
+* @brief example of PRE02-C. Macro replacement lists should be parenthesized
+* this macro uses paranthesis around its lists properly, no practical use in this specific program but the rec example is here
+*/
+#define SQUAREMACRO (I) ((I)*(I))
+int replList = 5
+int squareMacNum2 = SQUAREMACRO(repList);
+/*
+* @brief example of PRE12-C. Do not define unsafe macros
+* this inline function replaces an unsafe macro, no practical use in this specific program but the rec example is here
+*/
+static inline int inlineFunc(){
+    return 24;
+}
+
 #endif //BANK_H
